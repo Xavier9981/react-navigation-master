922b0e05fd896551769d89e58f4b10ad53712a99
Xavier K Muneku <xavier.muneku@gmail.com>
2023-09-15 16:31:01 +0200

    modified file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,44 +6,47 @@ import {BsFacebook} from "react-icons/bs";
 import {BsTwitter} from "react-icons/bs";
 import {BsLinkedin} from "react-icons/bs";
 import {BiLogoInstagramAlt} from "react-icons/bi";
+import logo from "../assets/icons/Logo.png"
 
 function Footer() {
   return (
     <FooterSection>
       <div className="footer-links">
         <div>
-          <figure>
-            <img src="" alt="logo" />
-          </figure>
+          <a href="/">
+            <figure>
+              <img src={logo} alt="Logo" srcset="" />
+            </figure>
+          </a>
         </div>
-        <div>
-          <ul>
+        <div className="wrap-footer-links">
+          <div>
             <h2>Institution Info</h2>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Terms and conditions</a></li>
-            <li><a href="#">Customer service</a></li>
-          </ul>
-        </div>
-        <div>
-          <ul>
+            <ul>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">Terms and conditions</a></li>
+              <li><a href="#">Customer service</a></li>
+            </ul>
+          </div>
+          <div>
             <h2>Top Links</h2>
-            <li><a href="#">Student Loans</a></li>
-            <li><a href="#">Repay Student Loan</a></li>
-            <li><a href="#">Scholarships</a></li>
-          </ul>
-        </div>
-        <div>
-          <ul>
+            <ul>
+              <li><a href="#">Student Loans</a></li>
+              <li><a href="#">Repay Student Loan</a></li>
+              <li><a href="#">Scholarships</a></li>
+            </ul>
+          </div>
+          <div>
             <h2>Social Links</h2>
-            <li><a href="#"><BsFacebook/></a></li>
-            <li><a href="#"><BsTwitter/></a></li>
-            <li><a href="#"><BsLinkedin/></a></li>
-            <li><a href="#"><BiLogoInstagramAlt/></a></li>
-          </ul>
+            <ul>
+              <li><a href="#"><BsFacebook/></a></li>
+              <li><a href="#"><BsTwitter/></a></li>
+              <li><a href="#"><BsLinkedin/></a></li>
+              <li><a href="#"><BiLogoInstagramAlt/></a></li>
+            </ul>
+          </div>
         </div>
-        <div></div>
-        <div></div>
       </div>
     </FooterSection>
   )
@@ -51,8 +54,57 @@ function Footer() {
 
 const FooterSection = styled.section`
   background-color: #18416d;
+  padding: 5rem 4rem;
+
+
   .footer-links{
     display: flex;
+    color: #fff;
+    justify-content: center;
+    gap: 5rem;
+
+    div:nth-child(1) figure{
+      background-color: #fff;
+      border-radius: 15px;
+    }
+
+    .wrap-footer-links {
+      display: flex;
+      gap: 5rem;
+
+      div{
+        h2{
+          margin-inline-start: -20px;
+        }
+        ul{
+          li{
+            margin-block-start: 10px;
+            a{
+              color: #fff;
+              text-decoration: none;
+            }
+          }
+        }
+      }
+
+      div:nth-child(3){
+        ul{
+          display: flex;
+          gap: 3rem;
+          list-style-type: none;
+          li{
+
+            a{
+              color: #fff;
+              text-decoration: none;
+              svg{
+                font-size: 24px;
+              }
+            }
+          }
+        }
+      }
+    }
   }
   
 
