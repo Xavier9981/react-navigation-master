be0526ce2a0162f2698bc5612866b016ca5c1457
Xavier K Muneku <xavier.muneku@gmail.com>
2023-10-20 15:43:58 +0200

    multiple changes made

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import Menu from "./Menu";
@@ -9,38 +9,57 @@ import { IoClose } from "react-icons/io5";
 
 const Navbar = ({ toggleDrawer, routes, navbarOpen, setNavbarOpen }) => {
   [navbarOpen, setNavbarOpen] = useState(true);
+
+  const [show, setShow] = useState(true)
+    const controlNavbar = () => {
+        if (window.scrollY > 100) {
+            setShow(false)
+        } else {
+            setShow(true)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', controlNavbar)
+        return () => {
+            window.removeEventListener('scroll', controlNavbar)
+        }
+    }, [])
+  
   return (
-    <SNavbar>
-      <NavContainer>
-        <DrawerButton onClick={toggleDrawer}>
-          <div
-            className="icon-wrapper"
-            onClick={() => setNavbarOpen((prev) => !prev)}
-          >
-            {navbarOpen ? <i className="hamburger"></i> : <IoClose />}
-          </div>
-        </DrawerButton>
-        <SNavbarBrand>
-          <a href="/">
-            <img src={logo} alt="Logo" srcset="" />
-          </a>
-        </SNavbarBrand>
-        <RightNav>
-          <NavRoutes>
-            {routes.map((route) => {
-              if (route.subRoutes) {
-                return <Menu route={route} key={route.name} />;
-              }
-              return (
-                <NavRoute to={route.link} key={route.name}>
-                  {route.name}
-                </NavRoute>
-              );
-            })}
-          </NavRoutes>
-        </RightNav>
-      </NavContainer>
-    </SNavbar>
+    <NavWrapper>
+      <nav className={`${show && 'nav__blue'}`}>
+        <NavContainer>
+          <DrawerButton onClick={toggleDrawer}>
+            <div
+              className="icon-wrapper"
+              onClick={() => setNavbarOpen((prev) => !prev)}
+            >
+              {navbarOpen ? <i className="hamburger"></i> : <IoClose />}
+            </div>
+          </DrawerButton>
+          <SNavbarBrand>
+            <a href="/">
+              <img src={logo} alt="Logo" srcset="" />
+            </a>
+          </SNavbarBrand>
+          <RightNav>
+            <NavRoutes>
+              {routes.map((route) => {
+                if (route.subRoutes) {
+                  return <Menu route={route} key={route.name} />;
+                }
+                return (
+                  <NavRoute to={route.link} key={route.name}>
+                    {route.name}
+                  </NavRoute>
+                );
+              })}
+            </NavRoutes>
+          </RightNav>
+        </NavContainer>
+      </nav>
+    </NavWrapper>
   );
 };
 
@@ -95,11 +114,26 @@ const DrawerButton = styled.button`
   }
 `;
 
-const SNavbar = styled.nav`
-  background-color: #ffffff;
-  position: sticky;
-  top: 0;
-  z-index: 9999;
+const NavWrapper = styled.div`
+  nav{
+    background-color: #ffffff;
+    position: fixed;
+    top: 0;
+    z-index: 9999;
+  }
+
+  .nav{
+    height:30px;
+    width: 100%;
+    position:fixed;
+    top:0;
+    transition-timing-function: ease-in;
+    transition:0.5s
+  }
+
+  .nav__blue{
+    background-color:blue;
+  }
 `;
 
 const NavContainer = styled.div`
